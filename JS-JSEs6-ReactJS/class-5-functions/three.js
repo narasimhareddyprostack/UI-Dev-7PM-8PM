1acc65f6ed256acf295d66765aef39eef2b7dfaa
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2026-06-30 09:44:49 +0530

    t

---
+++ JS-JSEs6-ReactJS/class-5-functions/three.js
@@ -0,0 +1,11 @@
+let employees=[
+    {"eid":1,"ename":"Manon","gender":"Genderfluid"},
+    {"eid":2,"ename":"Rianon","gender":"Female"},
+    {"eid":3,"ename":"Corbet","gender":"Male"},
+    {"eid":4,"ename":"Anjela","gender":"Female"},
+    {"eid":5,"ename":"Deerdre","gender":"Genderqueer"}
+]
+//print all employee names
+for(let emp of employees){
+    console.log(emp.ename)
+}
---
+++ JS-JSEs6-ReactJS/class-5-functions/two.js
@@ -0,0 +1,15 @@
+let eids=[101,102,103,104]
+//index    0   1  2   3
+console.log(eids[3])  //104
+console.log(eids[4])  //undefined
+/*  
+Iterate Array using 
+    a)for loop
+    b)while loop
+    c)do while loop
+    d)for of 
+*/
+
+for(let eid of eids){
+    console.log(eid)
+}
